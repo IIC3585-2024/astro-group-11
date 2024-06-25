d09eee9f6faafbe73be8be58545cbb452417edbc
Pablo Carrasco <pablo.carrasco@uc.cl>
2024-06-24 20:48:37 -0400

    Login con google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
